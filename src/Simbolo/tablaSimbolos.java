464893851cc9fc5eef6e72a7920dd3bef9ac69b4
Mario Balam <balammario200@gmail.com>
2025-12-16 10:23:29 -0600

    subiendo mandejo de datos y simbolos

---
+++ src/Simbolo/tablaSimbolos.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Simbolo;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author balam
+ */
+public class tablaSimbolos {
+    
+        private tablaSimbolos tablaAnterior;
+    private HashMap<String, Object> tablaActual;
+    private String nombre;
+    
+    public tablaSimbolos(){
+        this.tablaActual = new HashMap<>();
+        this.nombre = "";
+    }
+
+    public tablaSimbolos getTablaAnterior() {
+        return tablaAnterior;
+    }
+
+    public void setTablaAnterior(tablaSimbolos tablaAnterior) {
+        this.tablaAnterior = tablaAnterior;
+    }
+
+    public HashMap<String, Object> getTablaActual() {
+        return tablaActual;
+    }
+
+    public void setTablaActual(HashMap<String, Object> tablaActual) {
+        this.tablaActual = tablaActual;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public tablaSimbolos(tablaSimbolos tablaAnterior){
+        this.tablaAnterior = tablaAnterior;
+        this.tablaActual = new HashMap<>();       
+        this.nombre = "";
+    }
+    
+    public boolean setVariables(Simbolo simbolo){
+        Simbolo busqueda = (Simbolo) this.tablaActual.get(simbolo.getId().toLowerCase());
+        if (busqueda == null){
+            this.tablaActual.put(simbolo.getId().toLowerCase(), simbolo);
+            return true;
+        }
+        return false;
+    }
+    /*
+        var a: int = 0;
+        while(a<5){
+            var b: int = 0;
+            if(){
+            }
+            print(a);
+            a = a+1;
+        }
+    
+        */
+    public Simbolo getVariable(String id) {
+        for (tablaSimbolos i = this; i != null; i = i.getTablaAnterior()) {
+            Simbolo busqueda = (Simbolo) i.tablaActual.
+                    get(id.toLowerCase());
+            if (busqueda != null) {
+                return busqueda;
+            }
+        }
+        return null;
+    }
+    
+    public java.util.LinkedList<Simbolo> getVariables() {
+        java.util.LinkedList<Simbolo> lista = new java.util.LinkedList<>();
+        
+        // Recorremos todos los valores guardados en el entorno actual
+        for (Object valor : this.tablaActual.values()) {
+            // Validamos que sea un Simbolo antes de agregarlo
+            if (valor instanceof Simbolo) {
+                lista.add((Simbolo) valor);
+            }
+        }
+        return lista;
+    }
+
+   
+    
+    
+}
